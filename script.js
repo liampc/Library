3f7fc4e9d4bab7ba2c06bfdb56501f2256fb2aa6
Kathlia Carlos <lia.carlos23@gmail.com>
2020-06-04 10:35:26 +0800

    add addbook func

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ let myLibrary = [];
 function Book(){
 
 }
+
+
+function addBooktoLibrary(book){
+    book = prompt("Give me a book")
+    myLibrary.push(book)
+}
+
+addBooktoLibrary.prototype = Object.create(Book.prototype);
+
+console.log(myLibrary);
