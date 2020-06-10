d8fc8c9f7efe5cc729f8b30437b569f17f98cf31
Kathlia Carlos <lia.carlos23@gmail.com>
2020-06-10 14:44:24 +0800

    add render func

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const bookList = document.querySelector(".books")
 
-let myLibrary = [];
+let myLibrary = ["BOOK1", "book2", "book3"];
 
 function Book(){
 
@@ -15,3 +15,17 @@ function addBooktoLibrary(book){
 addBooktoLibrary.prototype = Object.create(Book.prototype);
 
 console.log(myLibrary);
+
+function render(arr){
+    for (let book of arr){
+        let div = document.createElement("div");
+        let text = document.createTextNode(book)
+        div.appendChild(text);
+        div.classList.add("book-card");
+        document.querySelector(".books-section").appendChild(div)
+
+    }
+}
+
+render(myLibrary)
+
