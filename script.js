3df3590662a83ae7f83a978594bf204e5c29368e
Kathlia Carlos <lia.carlos23@gmail.com>
2020-06-13 09:39:05 +0800

    add newbook func in js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const bookList = document.querySelector(".books")
+const newBookBtn = document.querySelector("button");
 
 let myLibrary = ["BOOK1", "book2", "book3"];
 
@@ -27,5 +28,38 @@ function render(arr){
     }
 }
 
+function newBook(){
+    let titleForm = document.createElement("input");
+    let authorForm = document.createElement("input");
+    let statusSelect = document.createElement("select");
+    let optRead = document.createElement("option");
+    let optToRead = document.createElement("option");
+    
+
+    setAttr(titleForm, {"name":"title-form", "placeholder": "Title"});
+    setAttr(authorForm, {"name":"author-form", "placeholder": "Author"});
+    setAttr(statusSelect, {"name": "status"});
+    setAttr(optRead, {"value": "read"})
+    setAttr(optToRead, {"value": "To-read"})
+   
+    optRead.text = "Read";
+    optToRead.text = "To Read";
+    statusSelect.options.add(optRead, 0);
+    statusSelect.options.add(optToRead, 1);
+   
+    document.querySelector(".input-section").append(titleForm, authorForm, statusSelect);
+    newBookBtn.removeEventListener("click", newBook)
+}
+
+function setAttr(elem, attrs){
+    for (let prop in attrs){
+        elem.setAttribute(prop,attrs[prop]);
+    }
+}
+
+
+newBookBtn.addEventListener("click", newBook)
+
 render(myLibrary)
+
 
