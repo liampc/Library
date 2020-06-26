2fd98102a3771be247a9e84683f338045e75947f
Kathlia Carlos <lia.carlos23@gmail.com>
2020-06-26 10:43:25 +0800

    add addbook func to savebtn

--- script.js
+++ script.js
@@ -13,15 +13,13 @@ function Book(title,author,status){
 }
 
 
-
 function addBooktoLibrary(){
     let title = inputTitle.value;
     let author = inputAuthor.value;
     let status = selectStatus.options[selectStatus.selectedIndex].value
     let newBook = new Book(title,author,status)
     myLibrary.push(newBook)
-    console.log(myLibrary);
+    console.log(myLibrary)
 }
 
-addBooktoLibrary.prototype = Object.create(Book.prototype);
-
+saveBtn.addEventListener("click", addBooktoLibrary)
