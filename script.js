048db878eb676afa0346a60404d830a017d2858b
Kathlia Carlos <lia.carlos23@gmail.com>
2020-06-26 10:13:19 +0800

    edit select and optins attr

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputTitle = document.querySelector("#input-title")
 const inputAuthor = document.querySelector("#input-author")
 const saveBtn = document.querySelector("#save-btn")
+const selectStatus = document.querySelector("#status")
 
 
 let myLibrary = [];
@@ -14,9 +15,12 @@ function Book(title,author,status){
 
 
 function addBooktoLibrary(){
-    let newBook = new Book("Harry Potter","J.K Rowling", "Read")
+    let title = inputTitle.value;
+    let author = inputAuthor.value;
+    let status = selectStatus.options[selectStatus.selectedIndex].value
+    let newBook = new Book(title,author,status)
     myLibrary.push(newBook)
-    console.log(myLibrary)
+    console.log(myLibrary);
 }
 
 addBooktoLibrary.prototype = Object.create(Book.prototype);
